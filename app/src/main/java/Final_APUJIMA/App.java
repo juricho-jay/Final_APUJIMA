ab14bfca6cde74a6e96d38aab9229b3bc571bb63
kimth1992 <kimth1992@naver.com>
2021-08-26 13:26:22 +0900

    ..

--- app/src/main/java/Final_APUJIMA/App.java
+++ app/src/main/java/Final_APUJIMA/App.java
@@ -4,11 +4,11 @@
 package Final_APUJIMA;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Welcome";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
