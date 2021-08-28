bf74df1acc41d9551df15fd6661eea676d059ca3
kimth1992 <kimth1992@naver.com>
2021-08-28 11:59:50 +0900

    introduce method modify

--- app/src/main/java/Final_APUJIMA/Main.java
+++ app/src/main/java/Final_APUJIMA/Main.java
@@ -207,10 +207,17 @@ public class Main {
 
   //로그인메뉴 메서드
   static void doIntroMenu() {
-    System.out.println("[소개] 페이지입니다.");
-    System.out.println("We always with you, 모든 사람들이 아프지 않길 바라는 커뮤니티, "
-        + "APUJIMA입니다.");
+    while(true) {
+      System.out.println("[소개] 페이지입니다.");
+      System.out.println("We always with you, 모든 사람들이 아프지 않길 바라는 커뮤니티, "
+          + "APUJIMA입니다.");
+      System.out.println();
 
+      System.out.println("0) 뒤로가기");
+      int num = Prompt.inputInt("번호> ");
+      if(num == 0)
+        return;
+    }
   }
   //회원가입메뉴 메서드
   static void doMedicineMenu() {
