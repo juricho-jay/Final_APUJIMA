ddb7817c2666d0634686d1d90fd8c519a88ebffb
shinhyunji1 <shji0814@gmail.com>
2021-09-09 19:15:20 +0900

    modify admin

--- app/src/main/java/pms/handler/AuthLoginHandler.java
+++ app/src/main/java/pms/handler/AuthLoginHandler.java
@@ -1,10 +1,11 @@
 package pms.handler;
 
 
+import java.sql.Date;
 import java.util.List;
+import Menu.Menu;
 import pms.domain.Member;
 import util.Prompt;
-import Menu.Menu;
 
 public class AuthLoginHandler implements Command {
 
@@ -40,7 +41,16 @@ public class AuthLoginHandler implements Command {
       Member root = new Member();
       root.setName("관리자");
       root.setEmail("admin@test.com");
+      root.setId("admin");
+      root.setPassword("0000");
+      root.setEmail("admin@test.com");
+      root.setBirthDay("0000-000--00");
+      root.setPhoneNum("010-0000-0000");
+      root.setPhoto("1.jpg");
+      root.setSex("여");
+      root.setRegisteredDate(new Date(System.currentTimeMillis()));
       loginUser = root;
+      memberList.add(loginUser);
       userAccessLevel = Menu.ACCESS_ADMIN | Menu.ACCESS_GENERAL;
       return;
     } 
