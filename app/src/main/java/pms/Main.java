d264a589ab49b739edf1db2f89652ca0c9252408
juricho-jay <juri.cho.jay@gmail.com>
2021-09-09 17:18:23 +0900

    menu advised

--- app/src/main/java/pms/Main.java
+++ app/src/main/java/pms/Main.java
@@ -39,7 +39,6 @@ import pms.handler.FreeBoardUpdateHandler;
 import pms.handler.IntroMenu;
 import pms.handler.MedicineAddHandler;
 import pms.handler.MedicineDeleteHandler;
-import pms.handler.MedicineDetailHandler;
 import pms.handler.MedicineListHandler;
 import pms.handler.MedicineSearchHandler;
 import pms.handler.MedicineUpdateHandler;
@@ -95,15 +94,24 @@ public class Main {
   }
 
   public Main() {
-    commandMap.put("/member/add", new MemberAddHandler(memberList));
-    commandMap.put("/member/list", new MemberListHandler(memberList));
+    //    commandMap.put("/admin/permission", new 구현예정);
+    //    commandMap.put("/admin/list", new 구현예정);
 
+    commandMap.put("/intro", new IntroMenu());
+
+    commandMap.put("/medicine/add", new MedicineAddHandler(medicineList));
+    //    commandMap.put("/medicine/request", new MedicineRequestHandler(requestList));
+    commandMap.put("/medicine/list", new MedicineListHandler(medicineList));
+    commandMap.put("/medicine/update", new MedicineUpdateHandler(medicineList));
+    commandMap.put("/medicine/delete", new MedicineDeleteHandler(medicineList));
+    commandMap.put("/medicine/search", new MedicineSearchHandler(medicineList));
 
     commandMap.put("/counselingMember/list", new DoctorMemberListHandler(memberList));
     commandMap.put("/counselingMember/add", new CounselingMemberAddHandler(counselingMemberList));
     commandMap.put("/counselingMember/myList", new CounselingMemberMyListHandler(counselingMemberList));
     commandMap.put("/counselingMember/doctorList", new CounselingMemberDoctorListHandler(counselingMemberList));
     // 바로 위에꺼 상담신청 이력
+
 
     commandMap.put("/noticeBoard/add", new NoticeBoardAddHandler(noticeBoardList));
     commandMap.put("/noticeBoard/list", new NoticeBoardListHandler(noticeBoardList));
@@ -112,14 +120,12 @@ public class Main {
     commandMap.put("/noticeBoard/delete", new NoticeBoardDeleteHandler(noticeBoardList));
     commandMap.put("/noticeBoard/search", new NoticeBoardSearchHandler(noticeBoardList));
 
-
     commandMap.put("/freeBoard/add", new FreeBoardAddHandler(freeBoardList));
     commandMap.put("/freeBoard/list", new FreeBoardListHandler(freeBoardList));
     commandMap.put("/freeBoard/detail", new FreeBoardDetailHandler(freeBoardList));
     commandMap.put("/freeBoard/update", new FreeBoardUpdateHandler(freeBoardList));
     commandMap.put("/freeBoard/delete", new FreeBoardDeleteHandler(freeBoardList));
     commandMap.put("/freeBoard/search", new FreeBoardSearchHandler(freeBoardList));
-
 
     commandMap.put("/doctorBoard/add", new DoctorBoardAddHandler(doctorBoardList));
     commandMap.put("/doctorBoard/list", new DoctorBoardListHandler(doctorBoardList));
@@ -128,22 +134,15 @@ public class Main {
     commandMap.put("/doctorBoard/delete", new DoctorBoardDeleteHandler(doctorBoardList));
     commandMap.put("/doctorBoard/search", new DoctorBoardSearchHandler(doctorBoardList));
 
-    commandMap.put("/medicine/add", new MedicineAddHandler(medicineList));
-    commandMap.put("/medicine/list", new MedicineListHandler(medicineList));
-    commandMap.put("/medicine/detail", new MedicineDetailHandler(medicineList));
-    commandMap.put("/medicine/update", new MedicineUpdateHandler(medicineList));
-    commandMap.put("/medicine/delete", new MedicineDeleteHandler(medicineList));
-    commandMap.put("/medicine/search", new MedicineSearchHandler(medicineList));
-
-
-
+    commandMap.put("/member/add", new MemberAddHandler(memberList));
+    commandMap.put("/member/list", new MemberListHandler(memberList));
 
     commandMap.put("/auth/login", new AuthLoginHandler(memberList));
     commandMap.put("/auth/logout", new AuthLogoutHandler());
     commandMap.put("/auth/userInfo", new AuthUserInfoHandler(memberList));
 
-    commandMap.put("/intro", new IntroMenu());
-    //    commandMap.put("/community/freeBoard", new FreeBoardHashMap(freeBoardList));
+
+
   }
 
 
@@ -215,19 +214,18 @@ public class Main {
     MenuGroup mainMenuGroup = new MenuGroup("WELCOME TO APUJIMA!");
     mainMenuGroup.setPrevMenuTitle("종료");
 
+    mainMenuGroup.add(createApprovalMenu());
+    mainMenuGroup.add(new MenuItem("소개", "/intro"));
+    mainMenuGroup.add(createMedicineMenu());
+    mainMenuGroup.add(createCounselingMenu());
+
+    mainMenuGroup.add(createCommunityMenu());
 
     mainMenuGroup.add(new MenuItem("로그인", Menu.ACCESS_LOGOUT, "/auth/login"));
     mainMenuGroup.add(new MenuItem("내정보", Menu.ACCESS_GENERAL, "/auth/userInfo"));
     mainMenuGroup.add(new MenuItem("로그아웃", Menu.ACCESS_GENERAL, "/auth/logout"));
 
-    mainMenuGroup.add(new MenuItem("소개", Menu.ACCESS_LOGOUT, "/intro"));
-    mainMenuGroup.add(createMedicineMenu());
-    mainMenuGroup.add(createCounselingMenu());
     mainMenuGroup.add(createMemberMenu());
-    mainMenuGroup.add(createNoticeMenu());
-    mainMenuGroup.add(createFreeBoardMenu());
-    mainMenuGroup.add(createDoctorBoardMenu());
-
 
 
     //    mainMenuGroup.add(createCommunityMenu());
@@ -236,6 +234,62 @@ public class Main {
     return mainMenuGroup;
   }
 
+
+  private Menu createApprovalMenu() {
+    MenuGroup approvalMenu = new MenuGroup("승인 관리", Menu.ACCESS_ADMIN);
+
+    approvalMenu.add(new MenuItem("승인 허가", Menu.ACCESS_ADMIN, "/admin/approval")); // AdminApprovalHandler
+    approvalMenu.add(new MenuItem("승인 내역", Menu.ACCESS_ADMIN,"/admin/list")); // AdminListHandler
+    return approvalMenu;
+  }
+
+
+  //소개는 바로 intro 연결
+
+  private Menu createMedicineMenu() {
+    MenuGroup medicineMenu = new MenuGroup("약국");
+
+    medicineMenu.add(new MenuItem("약품 목록", "/medicine/list"));
+    medicineMenu.add(new MenuItem("약품 추가", Menu.ACCESS_ADMIN, "/medicine/add"));
+    medicineMenu.add(new MenuItem("약품 등록 요청", Menu.ACCESS_DOCTOR, "/medicine/request"));
+    medicineMenu.add(new MenuItem("약품 수정", Menu.ACCESS_ADMIN, "/medicine/update"));
+    medicineMenu.add(new MenuItem("약품 삭제", Menu.ACCESS_ADMIN, "/medicine/delete"));
+    medicineMenu.add(new MenuItem("약품 검색", "/medicine/search"));
+
+    return medicineMenu;
+  }
+
+  private Menu createCounselingMenu() {
+    MenuGroup memberMenu = new MenuGroup("HEALER");
+    memberMenu.add(new MenuItem("의사 리스트", Menu.ACCESS_LOGOUT, "/counselingMember/list"));
+    memberMenu.add(new MenuItem("상담신청", Menu.ACCESS_GENERAL, "/counselingMember/add"));
+    memberMenu.add(new MenuItem("My 상담 목록", Menu.ACCESS_GENERAL, "/counselingMember/myList"));
+    memberMenu.add(new MenuItem("Healer 상담 목록", Menu.ACCESS_DOCTOR, "/counselingMember/doctorList"));
+
+    return memberMenu;
+  }
+
+  private Menu createCommunityMenu() {
+    MenuGroup communityMenu = new MenuGroup("커뮤니티");
+
+    communityMenu.add(createNoticeMenu());
+    communityMenu.add(createFreeBoardMenu());
+    communityMenu.add(createDoctorBoardMenu());
+
+    return communityMenu;
+  }
+
+  private Menu createNoticeMenu() {
+    MenuGroup noticeMenu = new MenuGroup("공지사항");
+
+    noticeMenu.add(new MenuItem("글쓰기", Menu.ACCESS_ADMIN, "/noticeBoard/add"));
+    noticeMenu.add(new MenuItem("목록", "/noticeBoard/list"));
+    noticeMenu.add(new MenuItem("상세보기", "/noticeBoard/detail"));
+    noticeMenu.add(new MenuItem("변경", Menu.ACCESS_ADMIN, "/noticeBoard/update"));
+    noticeMenu.add(new MenuItem("삭제", Menu.ACCESS_ADMIN, "/noticeBoard/delete"));
+    noticeMenu.add(new MenuItem("검색", "/noticeBoard/search"));
+    return noticeMenu;
+  }
 
   private Menu createFreeBoardMenu() {
     MenuGroup freeBoardMenu = new MenuGroup("APUs 자유게시판");
@@ -247,18 +301,6 @@ public class Main {
     freeBoardMenu.add(new MenuItem("삭제", Menu.ACCESS_GENERAL, "/freeBoard/delete"));
     freeBoardMenu.add(new MenuItem("검색", "/freeBoard/search"));
     return freeBoardMenu;
-  }
-
-  private Menu createNoticeMenu() {
-    MenuGroup noticeMenu = new MenuGroup("공지사항");
-
-    noticeMenu.add(new MenuItem("글쓰기", Menu.ACCESS_ADMIN, "/notice/add"));
-    noticeMenu.add(new MenuItem("목록", "/notice/list"));
-    noticeMenu.add(new MenuItem("상세보기", "/notice/detail"));
-    noticeMenu.add(new MenuItem("변경", Menu.ACCESS_ADMIN, "/notice/update"));
-    noticeMenu.add(new MenuItem("삭제", Menu.ACCESS_ADMIN, "/notice/delete"));
-    noticeMenu.add(new MenuItem("검색", "/notice/search"));
-    return noticeMenu;
   }
 
   private Menu createDoctorBoardMenu() {
@@ -286,26 +328,5 @@ public class Main {
     return memberMenu;
   }
 
-  private Menu createCounselingMenu() {
-    MenuGroup memberMenu = new MenuGroup("HEALER");
-    memberMenu.add(new MenuItem("의사 리스트", Menu.ACCESS_LOGOUT, "/counselingMember/list"));
-    memberMenu.add(new MenuItem("상담신청", Menu.ACCESS_GENERAL, "/counselingMember/add"));
-    memberMenu.add(new MenuItem("My 상담 목록", Menu.ACCESS_GENERAL, "/counselingMember/myList"));
-    memberMenu.add(new MenuItem("Healer 상담 목록", Menu.ACCESS_DOCTOR, "/counselingMember/doctorList"));
 
-    return memberMenu;
-  }
-
-  private Menu createMedicineMenu() {
-    MenuGroup medicineMenu = new MenuGroup("약국");
-
-    medicineMenu.add(new MenuItem("약품 목록", "/medicine/list"));
-    medicineMenu.add(new MenuItem("약품 추가",  "/medicine/add"));
-    medicineMenu.add(new MenuItem("약품 수정", Menu.ACCESS_ADMIN, "/medicine/update"));
-    medicineMenu.add(new MenuItem("약품 삭제", Menu.ACCESS_ADMIN, "/medicine/delete"));
-    medicineMenu.add(new MenuItem("약품 검색", Menu.ACCESS_GENERAL | Menu.ACCESS_LOGOUT, "/medicine/search"));
-    medicineMenu.add(new MenuItem("약품 상세보기", Menu.ACCESS_GENERAL | Menu.ACCESS_LOGOUT, "/medicine/detail"));
-
-    return medicineMenu;
-  }
 }
