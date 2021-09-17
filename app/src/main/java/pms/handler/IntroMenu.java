47c4ff17c0ef92eb48defe2301c199cfd47b3a67
shinhyunji1 <shji0814@gmail.com>
2021-09-17 17:44:05 +0900

    hj

--- app/src/main/java/pms/handler/IntroMenu.java
+++ app/src/main/java/pms/handler/IntroMenu.java
@@ -8,6 +8,15 @@ public class IntroMenu implements Command {
     System.out.println("[소개] 페이지입니다.");
     System.out.println("We always with you, 모든 사람들이 아프지 않길 바라는 커뮤니티, "
         + "APUJIMA입니다.");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+
     System.out.println();
 
     //      int num = Prompt.inputInt("번호> ");
