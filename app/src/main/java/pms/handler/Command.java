319628ab18dfd7dbc5edb3d368f43be198da0469
juricho-jay <juri.cho.jay@gmail.com>
2021-09-18 16:24:41 +0900

    .

--- app/src/main/java/pms/handler/Command.java
+++ app/src/main/java/pms/handler/Command.java
@@ -2,6 +2,6 @@ package pms.handler;
 
 public interface Command {
   void execute(CommandRequest request) throws Exception;
-  // 구현 클래스에서 implements Command 써야함
+  // 구현 클래스에서 implements Command request 써야함
 
 }
