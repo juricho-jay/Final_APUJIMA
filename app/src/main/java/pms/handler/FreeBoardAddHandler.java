e404c10202f1e8f9ff47663e95b03ca0285c3edf
shinhyunji1 <shji0814@gmail.com>
2021-09-23 19:47:39 +0900

    .

--- app/src/main/java/pms/handler/FreeBoardAddHandler.java
+++ app/src/main/java/pms/handler/FreeBoardAddHandler.java
@@ -7,6 +7,8 @@ import util.Prompt;
 
 public class FreeBoardAddHandler extends AbstractFreeBoardHandler{
 
+  static int lastNo;// 게시물 마지막 번호
+
   public FreeBoardAddHandler(List<FreeBoard> freeBoardList) {
     super(freeBoardList);
   }
@@ -18,7 +20,23 @@ public class FreeBoardAddHandler extends AbstractFreeBoardHandler{
     System.out.println();
     FreeBoard freeBoard = new FreeBoard();
 
-    freeBoard.setNo(FreeBoard.lastIndex++);
+    if (freeBoardList.size() == 0) {// 게시물이 0개일 경우
+      //      lastNo = FreeBoard.lastIndex;
+      freeBoard.setNo(FreeBoard.lastIndex);
+      FreeBoard.lastIndex++;
+      //      lastNo = FreeBoard.lastIndex;
+      return;
+    } else { // 게시물 1개 이상일 경우
+      if (freeBoard.getNo() == freeBoardList.size()) { // 현재 게시물번호와 총게시물개수와 같다면
+        FreeBoard.lastIndex = freeBoard.getNo();
+        lastNo = FreeBoard.lastIndex;
+        lastNo++;
+        FreeBoard.lastIndex = lastNo;
+        freeBoard.setNo(FreeBoard.lastIndex);
+        FreeBoard.lastIndex++;
+      }
+
+    }
     freeBoard.setTitle(Prompt.inputString("제목> "));
     freeBoard.setContent(Prompt.inputString("내용> "));
 
