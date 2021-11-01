2f70abcfa680c3b84966359269adbac2a93b2a42
juricho-jay <juri.cho.jay@gmail.com>
2021-11-01 19:42:25 +0900

    >

---
+++ app-server/src/main/java/apus/dao/MemberDao.java
@@ -0,0 +1,22 @@
+package apus.dao;
+
+import java.util.List;
+import apus.domain.Member;
+
+public interface MemberDao {
+  void insert(Member member) throws Exception;
+  void insert2(Member member) throws Exception;
+  List<Member> findAll() throws Exception;
+  List<Member> findDoctor() throws Exception;
+  List<Member> findMan() throws Exception;
+  List<Member> findWoman() throws Exception;
+  //  Member findByNo(int no) throws Exception;
+  Member findByName(String name) throws Exception;
+  Member findById(String id) throws Exception;
+  Member findByIdPwd(String id, String password) throws Exception;
+  void update(Member member) throws Exception;
+  void update2(Member member) throws Exception;
+  void delete(String id) throws Exception;
+  Member check(Member member) throws Exception;
+  Member findDoctorMajor(Member member) throws Exception;
+}
---
+++ app-server/src/main/java/apus/domain/Doctor.java
@@ -0,0 +1,45 @@
+package apus.domain;
+
+import java.io.Serializable;
+
+public class Doctor implements Serializable{
+
+  private int no;
+  private String major;
+  private String license;
+  private String homepage;
+  private String introduction;
+
+
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getMajor() {
+    return major;
+  }
+  public void setMajor(String major) {
+    this.major = major;
+  }
+  public String getLicense() {
+    return license;
+  }
+  public void setLicense(String license) {
+    this.license = license;
+  }
+  public String getHomepage() {
+    return homepage;
+  }
+  public void setHomepage(String homepage) {
+    this.homepage = homepage;
+  }
+  public String getIntroduction() {
+    return introduction;
+  }
+  public void setIntroduction(String introduction) {
+    this.introduction = introduction;
+  }
+
+}
