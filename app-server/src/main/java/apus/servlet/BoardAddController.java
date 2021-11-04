80c575b867ea09987b5642599d9209fc841a4972
rollingball211 <jingoo211@gmail.com>
2021-11-04 21:18:09 +0900

    addboardwebs

---
+++ app-server/src/main/java/apus/servlet/BoardAddController.java
@@ -0,0 +1,84 @@
+package apus.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+import apus.dao.BoardDao;
+import apus.dao.MemberDao;
+import apus.domain.Board;
+import apus.domain.Member;
+
+@WebServlet("/board/add")
+public class BoardAddController extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  BoardDao boardDao;
+  MemberDao memberDao;
+  SqlSession sqlSession;
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
+    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
+    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
+    boardDao = (BoardDao) 웹애플리케이션공용저장소.getAttribute("boardDao");
+
+  }
+
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    Member writer = memberDao.findById(AuthLoginController.getLoginUser().getId());
+    Board board = new Board();
+
+    //    String convertDate = request.getParameter("birthDay");
+    //    SimpleDateFormat convertDate2 = new SimpleDateFormat("yyyy-MM-dd");
+    //
+    //    Date bDate = convertDate2.parse(convertDate);
+
+    board.setTitle(request.getParameter("title"));
+    board.setWriter(writer);
+    board.setContent(request.getParameter("content"));
+    /*    
+   board.setWhichBoard(request.getParameter("whichBoard"));
+    board.setRegisteredDate(request.getParameter("date"));
+     내일 질문하기.
+     */
+
+    /*
+     board.setComment/like.. detail에서 보여주는게 맞지않나../
+     */
+
+
+
+    try {
+      boardDao.insert(board);
+      sqlSession.commit();
+      response.setHeader("Refresh", "1;url=list");
+      request.getRequestDispatcher("MemberAdd.jsp").forward(request, response);
+
+    } catch (Exception e) {
+      // 오류를 출력할 때 사용할 수 있도록 예외 객체를 저장소에 보관한다.
+      request.setAttribute("error", e);
+
+      // 오류가 발생하면, 오류 내용을 출력할 뷰를 호출한다.
+      RequestDispatcher 요청배달자 = request.getRequestDispatcher("/Error.jsp");
+      요청배달자.forward(request, response);
+
+
+
+
+
+
+
+    }
+  }
+}
---
+++ app-server/src/main/java/apus/servlet/BoardDeleteController.java
@@ -0,0 +1,52 @@
+package apus.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+import apus.dao.BoardDao;
+import apus.domain.Board;
+
+@WebServlet("/board/delete")
+public class BoardDeleteController extends HttpServlet{
+  private static final long serialVersionUID = 1L;
+
+  SqlSession sqlSession;
+  BoardDao boardDao;
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
+    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
+    boardDao = (BoardDao) 웹애플리케이션공용저장소.getAttribute("boardDao");
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    try {
+      int no = Integer.parseInt(request.getParameter("no"));
+
+      Board board = boardDao.findByNo(no);
+
+      if (board == null) {
+        throw new Exception("해당 아이디의 회원이 없습니다.");
+      }
+
+      boardDao.delete(no);
+      sqlSession.commit();
+
+      response.sendRedirect("list");
+
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/Error.jsp").forward(request, response);
+    }
+  }
+}
